2b8742674ed64fb093665310b6c42d5aad990def
Fahim foysal Rabby <ffoysal6@gmail.com>
2020-12-21 02:17:15 +0600

    'Task' model & migration for 'tasks' table

---
+++ database/migrations/2020_12_20_135409_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+//            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreignId('user_id')->constrained();
+            $table->text('title');
+            $table->text('description')->nullable();
+            $table->integer('order');
+            $table->timestamp('created');
+            $table->timestamp('end_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
