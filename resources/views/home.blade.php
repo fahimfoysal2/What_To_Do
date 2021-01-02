e833016a262c39724646cea969af466564ec43cc
Fahim foysal Rabby <ffoysal6@gmail.com>
2021-01-02 21:09:03 +0600

    separation of task list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,9 @@
 @extends('layouts.app')
 
+@php
+    $pageTitle = "Dashboard: All Tasks";
+@endphp
+
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard: All Tasks') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                    <!-- {{ __('You are logged in and have tasks') }} -->
-                        @if (count($tasks) > 0)
-                            @foreach($tasks as $task)
-                                {{'Due :'.$task["end_time"] }} <br/>
-                                {{ $task["title"] }} <br/>
-                                {{ $task["description"] }}
-                                <hr>
-                            @endforeach
-                        @else
-                            <b>Task 0 :</b> <a href="{{route('task.create')}}">Create a new Task</a>, Now!!
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('includes.tasks.taskList')
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,36 +32,12 @@
         </div>
 </div>-->
 
+    @php
+        $pageTitle = "My Recent Tasks";
+    @endphp
+
     @auth
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">{{ __('My Recent Tasks') }}</div>
-
-                        <div class="card-body">
-                            @if (session('status'))
-                                <div class="alert alert-success" role="alert">
-                                    {{ session('status') }}
-                                </div>
-                            @endif
-
-                        <!-- {{ __('You are logged in and have tasks') }} -->
-                            @if (count($tasks) > 0)
-                                @foreach($tasks as $task)
-                                    {{'Due :'.$task["end_time"] }} <br/>
-                                    {{ $task["title"] }} <br/>
-                                    {{ $task["description"] }}
-                                    <hr>
-                                @endforeach
-                            @else
-                                <b>Task 0 :</b> <a href="{{route('task.create')}}">Create a new Task</a>, Now!!
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('includes.tasks.taskList')
     @endauth
 
     @guest
