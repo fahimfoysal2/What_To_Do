3c450d0121b998d23f8b75c13eaa87d7bdb8d779
Fahim foysal Rabby <ffoysal6@gmail.com>
2021-01-06 23:37:49 +0600

    + settings update notice

--- resources/views/setting.blade.php
+++ resources/views/setting.blade.php
@@ -5,16 +5,20 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
                 <div class="card bg-{{$theme_name ?? 'light'}}">
                     <div class="card-header bg-light">{{ __('Settings') }}</div>
 
                     <div class="card-body bg-{{$theme_name ?? 'light'}} text-success">
 
-
                         <form method="POST" action="{{ route('setting.save') }}">
-
                             @csrf
-
                             @foreach(config('enums.settings') as $setting_name => $setting_data)
                                 <div class="form-group row">
                                     <label for="end_time"
